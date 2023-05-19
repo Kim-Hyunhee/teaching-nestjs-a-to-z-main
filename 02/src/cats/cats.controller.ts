02833c84df452d0114d5fc62feecea1f340be0cf
Kim-Hyunhee <tta_bi@naver.com>
2023-05-19 12:20:29 +0900

    삭제

--- 02/src/cats/cats.controller.ts
+++ 02/src/cats/cats.controller.ts
@@ -4,8 +4,6 @@ import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 import { CatsService } from './cats.service';
 import { CatRequestDto } from './dto/cats.request.dto';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { ReadOnlyCatDto } from './dto/cat.dto';
 
 @Controller('cats')
 @UseInterceptors(SuccessInterceptor)
@@ -13,42 +11,29 @@ import { ReadOnlyCatDto } from './dto/cat.dto';
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
-  @ApiOperation({ summary: '현재 고양이 가져오기' })
   @Get()
   getCurrentCat() {
     return 'current cat';
   }
 
-  @ApiResponse({
-    status: 500,
-    description: 'Server Error ...',
-  })
-  @ApiResponse({
-    status: 200,
-    description: '성공',
-    type: ReadOnlyCatDto,
-  })
-  @ApiOperation({ summary: '회원가입' })
   @Post()
   async signUp(@Body() body: CatRequestDto) {
-    return await this.catsService.signUp(body);
+    console.log(body);
+    return 'signup';
   }
 
-  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn() {
     return 'login';
   }
 
-  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
-  updatePartialCat() {
+  logOut() {
     return 'logout';
   }
 
-  @ApiOperation({ summary: '고양이 이미지 업로드' })
   @Post('upload/cats')
-  deleteCat() {
+  uploadCatImg() {
     return 'uploadImg';
   }
 }
