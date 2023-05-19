73d9a2a3b04bf280d037b1f7b922f23a4d37e163
Kim-Hyunhee <tta_bi@naver.com>
2023-05-19 11:49:30 +0900

    api 문서 제작

--- 04 NestJS 개요 및 객체지향 디자인 패턴/src/cats/cats.schema.ts
+++ 04 NestJS 개요 및 객체지향 디자인 패턴/src/cats/cats.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { SchemaOptions, Document } from 'mongoose';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 const options: SchemaOptions = {
   timestamps: true,
@@ -8,21 +9,40 @@ const options: SchemaOptions = {
 
 @Schema(options)
 export class Cat extends Document {
+  @ApiProperty({
+    example: 'amamov@kakao.com',
+    description: 'email',
+    required: true,
+  })
   @Prop({ required: true, unique: true })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: 'blue',
+    description: 'name',
+    required: true,
+  })
   @Prop({ required: true })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    example: 'asdasdasdasd',
+    description: 'password',
+    required: true,
+  })
   @Prop({ required: true })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    example: 'asdasdasdasd',
+    description: 'image',
+  })
   @Prop()
   @IsString()
   imgUrl: string;
